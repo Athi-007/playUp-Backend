93bc4aefda74d1146598db6a8a39e67575d0af6d
Athithiyan-arch <athithiyanm87@gmail.com>
2026-01-11 11:27:29 +0530

    fix the route

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './src/config/database.js';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./src/config/database.js";
+import authRouter from "./src/routes/authRoute.js";
+
+dotenv.config();
 
 const app = express();
-app.use(express.json())
-dotenv.config();
-app.use(cors())
 
-import authRouter from './src/routes/authRoute.js';
+app.use(cors());
+app.use(express.json());
 
-app.use("/" , authRouter);
+app.get("/api", (req, res) => {
+  res.json({ status: "Backend is live" });
+});
 
+app.use("/auth", authRouter);
 
-
-
-connectDB().then(()=> {
-    app.listen(process.env.PORT , () => {   
-    console.log ("server listen on the port" )
+connectDB().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
   });
 });
 
